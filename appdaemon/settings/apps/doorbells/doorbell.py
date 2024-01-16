032d098afc8d0421ab018e3af238895b2f93c746
Your Name <you@example.com>
2024-01-16 05:44:35 +0000

    updating configuration

--- appdaemon/settings/apps/doorbells/doorbell.py
+++ appdaemon/settings/apps/doorbells/doorbell.py
@@ -17,25 +17,27 @@ import os
 #   Initial Version
 
 class Doorbell(hass.Hass):
-  ringtone = 10
-  gw_mac = ''
   def initialize(self):
-    if "notify" not in self.args or "sensor" not in self.args:
-      self.error("Please provide notify, sensor in config!")
+    if "notify" not in self.args:
+      self.error("Please provide notify, sensor, camera in config!")
       return
-    if "gw_mac" in self.args:
-      self.gw_mac = self.args['gw_mac']
+    self.listen_event(self.ha_event, "doorbell_call")
 
-    if 'ringtone' in self.args:
-      self.ringtone = self.args['ringtone']
+  def ha_event(self, event_name, data, kwargs):
+    self.log('{}:{}'.format(event_name, data))
 
-    self.listen_state(self.sensor_trigger, self.args['sensor'])
-
-  def sensor_trigger(self, entity, attribute, old, new, kwargs):
-    if new == None or new == "None" or new == "" or new == "unknown":
-      return
-    self.log("doorbell call")
-    self.log("old: {}, new: {}".format(old, new))
-    self.call_service("xiaomi_aqara/play_ringtone", gw_mac=self.gw_mac, ringtone_id=self.ringtone, ringtone_vol=100)
     self.notify("Звонок в дверь!!!", name = self.args['notify'])
-    # result = self.send_video()
+    if 'doorbell_pic' in data:
+      extra_data = {'photo': {'url':data['doorbell_pic'], 'capture': 'Фото звонившего.'}}
+      self.notify("Фото звонившего", name = self.args['notify'], data=extra_data)
+
+    self.say('Звонок в дверь')
+
+  def command(self, command):
+    self.call_service('yandex_station/send_command', command='sendText', text=command)
+
+  def say(self, command):
+    if 'alices' in self.args:
+      for alice in self.args['alices']:        
+        self.log('command = {}'.format(command))
+        self.call_service('media_player/play_media', entity_id=alice, media_content_type='text', media_content_id=command)
